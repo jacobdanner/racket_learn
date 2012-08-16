8a2cc966947c19f87bc4456c9e7cb7a735eef93d
Jacob Danner <jacob.danner@gmail.com>
2012-08-15 19:04:19 -0700

    macros and syntax, wow that's cool

--- quickstart1.racket.rkt
+++ quickstart1.racket.rkt
@@ -27,3 +27,45 @@
 
 (define (series mk)
   (hc-append 4 (mk 5) (mk 10) (mk 20)))
+
+; lambda == anonymous function
+; long form of the above
+(define verbose_series
+  (lambda (mk)
+    (hc-append 4 (mk 5) (mk 10) (mk 20))))
+
+
+; lexical scope - identifier == expression/binding
+(define (rgb-series mk)
+  (vc-append
+   (series (lambda (sz) (colorize (mk sz) "red")))
+   (series (lambda (sz) (colorize (mk sz) "green")))
+   (series (lambda (sz) (colorize (mk sz) "blue")))))
+
+(define (rgb-maker mk)
+  (lambda (sz)
+    (vc-append (colorize (mk sz) "red")
+               (colorize (mk sz) "green")
+               (colorize (mk sz) "blue"))))
+
+; use map to iterate over a list/apply
+(define (rainbow p)
+  (map (lambda (color)
+         (colorize p color))
+       (list "red" "orange" "yellow" "green" "blue" "purple")))
+
+; apply works on a list, but all at once instead of iteratively
+(define (vert_rainbow p)
+  (apply vc-append (rainbow p)))
+
+(require slideshow/flash)
+(define (simple_flash x y)
+  (filled-flash x y))
+
+(require slideshow/code)
+(define-syntax pict+code
+  (syntax-rules ()
+    [(pict+code expr)
+     (hc-append 10
+                expr
+                (code expr))]))
